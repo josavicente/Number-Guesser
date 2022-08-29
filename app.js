b214d8720514597ccaeb5c196e8e7584c13bd548
Josa Vicente <jvicenpe@gmail.com>
2022-08-29 20:54:34 +0200

    :tada:

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 
-document.getElementById('guess-value').addEventListener('submit', checkNumber);
+document.getElementById('guess-btn').addEventListener('submit', checkNumber);
 
-const guessNumber = document.getElementById('guess-input');
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessNumber = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+      
 const numberToGuest = Math.floor(Math.random() * 10) + 1;
 
 let   min = 1,
       max = 10,
-      numberOfGuests = 
-      remainingGuests = 
+      remainingGuests = 3;
+
 
 function checkNumber(e){
       if (guessNumber.value === numberToGuest){
