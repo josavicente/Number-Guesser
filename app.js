a0ff59151b8925042a0601f8b39750cce498309d
Josa Vicente <jvicenpe@gmail.com>
2022-08-29 22:26:21 +0200

    :sparkles:

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const game = document.querySelector('#game'),
       minNum = document.querySelector('.min-num'),
       maxNum = document.querySelector('.max-num'),
       guessBtn = document.querySelector('#guess-btn'),
-      guessNumber = document.querySelector('.guess-input'),
+      guessNumber = document.querySelector('#guess-input'),
       message = document.querySelector('.message');
 
       // Assign min and max UI
@@ -17,28 +17,40 @@ minNum.textContent = min;
 maxNum.textContent = max;
 //Listen Submit
 
-guessBtn.addEventListener('submit', checkNumber);
+guessBtn.addEventListener('click', checkNumber);
 
 const numberToGuest = Math.floor(Math.random() * 10) + 1;
-
+console.log(numberToGuest);
 
 
 
 function checkNumber(e){
-      const guess = parseInt(guessNumber.value);
-      if ( isNaN(guess) || guess < min || guess > max ) {
-            setMessage(`Please enter a number between ${min} and ${max}`);
+      let guess = parseInt(guessNumber.value);
+      if (isNaN(guess) || guess < min || guess > max ){
+            setMessage(`Please enter a number between ${min} and ${max}`, 'red');
       } else {
-            if (guessNumber.value === numberToGuest){
-                  console.log('Correct Number');
+            if (guess === numberToGuest){
+                  showGuessResult(`${numberToGuest} is correct. You win!`, 'green');
             }else{
-                  console.log('Incorrect number');
+                  remainingGuests -=1;
+                  if (remainingGuests === 0){
+                        showGuessResult(`Game Over! The correct number is: ${numberToGuest}`, 'red');
+                  }else{
+                        setMessage(`${guess} is incorrect. Remaining tries: ${remainingGuests}`, 'red');
+                  }
+                  
             }
       }
-      
 };
 
-function setMessage(message) {
-      message.textContent = message;
-      message.style.color = 'red';
+
+function showGuessResult(msg, color) {
+      guessNumber.disabled = true;
+      guessNumber.style.borderColor = color;
+      setMessage(msg,color);
+}
+
+function setMessage(msg, color) {
+      message.textContent = msg;
+      message.style.color = color;
 };
